fd644e1cfafd98f061403115a66ba60078be02a7
thorcc <thorchrcow@gmail.com>
2022-03-02 09:59:11 +0100

    remove need for git and node

--- README.md
+++ README.md
@@ -6,14 +6,8 @@
 
 Flask-VGS er en utvidelse til VS Code, laget for programfagene IT1 og IT2 på VGS. 
 
-Ved kjøring av `Create Flask` kopieres malen `https://github.com/thorcc/flask-it1`. 
-
 ## Dette trenger du:
 
-- Python (3.x.x)
-- [Node](https://nodejs.org/en/)
-- Git
-  - [Git (mac)](https://sourceforge.net/projects/git-osx-installer/)
-  - [Git (windows)](https://git-scm.com/download/windows)
+- [Python versjon 3](https://www.python.org/downloads/)
 
-> Restart VS Code etter du har installert python og git.
+> Det kan være du må restarte PC-en etter du har installert Python
--- extension.js
+++ extension.js
@@ -2,6 +2,42 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 const process = require("process");
+const fs = require('fs')
+
+
+const htmlTemplate = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flask - VGS</title>
+</head>
+<body>
+    <h1>Hallo, {{navn}}!</h1>
+</body>
+</html>
+
+`
+
+const appTemplate = `from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    navn = "Sandvika"
+    return render_template("index.html", navn=navn)
+
+`
+
+const requirementsTemplate = `click==8.0.4
+Flask==2.0.3
+itsdangerous==2.1.0
+Jinja2==3.0.3
+MarkupSafe==2.1.0
+Werkzeug==2.0.3
+`
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,24 +46,34 @@ const process = require("process");
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  const createFlask = vscode.commands.registerCommand("flask-vgs.createFlask", (uri) => {
+  const createFlask = vscode.commands.registerCommand("flask-vgs.createFlask", async uri => {
       // Kill all running terminals
       vscode.window.terminals.forEach((t) => {
         t.dispose();
       });
 
+      // Create Flask template
+      await fs.mkdir(`${uri.fsPath}/templates`, err => console.log(err));
+      await fs.writeFile(`${uri.fsPath}/templates/index.html`, htmlTemplate ,err => console.log(err));
+      await fs.writeFile(`${uri.fsPath}/app.py`, appTemplate ,err => console.log(err));
+      await fs.writeFile(`${uri.fsPath}/requirements.txt`, requirementsTemplate ,err => console.log(err));
+      
+      // Create virtual environment
       const terminal = vscode.window.createTerminal("flask-terminal");
       terminal.show();
       terminal.sendText(`cd "${uri.fsPath}"`);
-      terminal.sendText("npx degit https://github.com/thorcc/flask-it1 .");
-      if (process.platform == "win32") {
+      if (process.platform == "win32") { // win32 = windows
         terminal.sendText("py -3 -m venv venv");
         terminal.sendText("venv\\Scripts\\activate");
       } else {
         terminal.sendText("python3 -m venv venv");
         terminal.sendText(". venv/bin/activate");
       }
+
+      // Install Python libraries
       terminal.sendText("pip install -r requirements.txt");
+
+      // Run Flask
       terminal.sendText("flask run");
     }
   );
@@ -37,15 +83,17 @@ function activate(context) {
         t.dispose();
       });
 
+      // Activate environment
       const terminal = vscode.window.createTerminal("flask-terminal");
       terminal.show();
       terminal.sendText(`cd "${uri.fsPath}"`);
-      terminal.sendText("npx degit https://github.com/thorcc/flask-it1 .");
       if (process.platform == "win32") {
         terminal.sendText("venv\\Scripts\\activate");
       } else {
         terminal.sendText(". venv/bin/activate");
       }
+
+      // Run Flask
       terminal.sendText("flask run");
     }
   );
